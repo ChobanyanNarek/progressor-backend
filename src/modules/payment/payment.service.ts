98c6c99d4369252f9cd7a2e4fac2e62e794da220
narchobanyan <narek.chobanyan@moneteam.com>
2026-08-14 16:15:39 +0400

    fix: use servicestest pay URL which correctly redirects to arca card form

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -20,7 +20,7 @@ const AMERIA_BASE_URL = IS_TEST
   ? 'https://servicestest.ameriabank.am/VPOS'
   : 'https://services.ameriabank.am/VPOS';
 const AMERIA_PAY_URL = IS_TEST
-  ? 'https://vpos-epg.test.ameriabank.am/payments/pay'
+  ? 'https://servicestest.ameriabank.am/VPOS/Payments/Pay'
   : 'https://payments.ameriabank.am/forms/frm_paymentspage.aspx';
 
 // Monthly price in AMD
